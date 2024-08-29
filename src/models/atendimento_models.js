1be965a56df07e6a5247b380951284482877cb02
Reinald Mendes <112445979+ReinaldMendes@users.noreply.github.com>
2024-08-29 09:33:23 -0300

    Update atendimento_models.js

--- src/models/atendimento_models.js
+++ src/models/atendimento_models.js
@@ -6,20 +6,37 @@ const model = (atendimento, id = nextId++) => {
     clientes_controller.show(atendimento.id_cliente) &&
     atendimento.servicos !== undefined
   ) {
+    let horarioInicio = new Date(`1970-01-01T${atendimento.horario_inicio}:00`);
+    let horarioFim = new Date(horarioInicio);  // Cópia da data de início
+    let precoTotal = 0;
+
     const servicosComNotas = atendimento.servicos.map(servico => {
-      return {
-        id_servico: servico.id_servico,
-        nota: servico.nota
-      };
-    });
+      const servicoData = servicos_controller.show(servico.id_servico);
+      
+      if (servicoData) {
+        // Incrementa o horário fim com a duração do serviço
+        horarioFim.setMinutes(horarioFim.getMinutes() + servicoData.duracao);
+
+        // Soma o preço do serviço ao preço total
+        precoTotal += servicoData.preco;
+
+        return {
+          id_servico: servico.id_servico,
+          nota: servico.nota,
+          nome: servicoData.nome,
+          duracao: servicoData.duracao,
+          preco: servicoData.preco
+        };
+      }
+    }).filter(servico => servico !== undefined);  // Remove serviços inválidos
 
     return {
       id,
       id_cliente: atendimento.id_cliente,
       servicos: servicosComNotas,
       horario_inicio: atendimento.horario_inicio,
-      horario_fim: atendimento.horario_fim,
-      preco: atendimento.preco,
+      horario_fim: horarioFim.toTimeString().substr(0, 5),  // Retorna apenas HH:mm
+      preco: precoTotal.toFixed(2),
       status_pagamento: atendimento.status_pagamento,
     };
   }
