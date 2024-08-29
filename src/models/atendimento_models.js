f46e4fbd85086ae4709adbe0b0f4f2df91c3a399
Reinald Mendes <112445979+ReinaldMendes@users.noreply.github.com>
2024-08-29 08:36:17 -0300

    Update atendimento_models.js

--- src/models/atendimento_models.js
+++ src/models/atendimento_models.js
@@ -2,6 +2,40 @@ let nextId = 1;
 
 const model = (atendimento, id = nextId++) => {
   if (
+    atendimento.id_cliente !== undefined &&
+    clientes_controller.show(atendimento.id_cliente) &&
+    atendimento.servicos !== undefined &&
+    atendimento.notas !== undefined
+  ) {
+    const servicos = [];
+
+    atendimento.servicos.forEach((id_servico, index) => {
+      if (servicos_controller.show(id_servico)) {
+        servicos.push({
+          id_servico,
+          nota: atendimento.notas[index],
+        });
+      }
+    });
+
+    return {
+      id,
+      id_cliente: atendimento.id_cliente,
+      servicos,
+      horario_inicio: atendimento.horario_inicio,
+      horario_fim: atendimento.horario_fim,
+      preco: atendimento.preco,
+      status_pagamento: atendimento.status_pagamento,
+    };
+  }
+};
+
+
+
+let nextId = 1;
+
+const model = (atendimento, id = nextId++) => {
+  if (
     atendimento.id_cliente != undefined &&
     clientes_controller.show(atendimento.id_cliente) &&
     atendimento.id_servico != undefined &&
