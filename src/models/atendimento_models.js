327cf24d996c3e3aa28b4ec4a190474c87b4d63d
Reinald Mendes <112445979+ReinaldMendes@users.noreply.github.com>
2024-08-29 09:17:18 -0300

    Update atendimento_models.js

--- src/models/atendimento_models.js
+++ src/models/atendimento_models.js
@@ -4,53 +4,23 @@ const model = (atendimento, id = nextId++) => {
   if (
     atendimento.id_cliente !== undefined &&
     clientes_controller.show(atendimento.id_cliente) &&
-    atendimento.servicos !== undefined &&
-    atendimento.notas !== undefined
+    atendimento.servicos !== undefined
   ) {
-    const servicos = [];
-
-    atendimento.servicos.forEach((id_servico, index) => {
-      if (servicos_controller.show(id_servico)) {
-        servicos.push({
-          id_servico,
-          nota: atendimento.notas[index],
-        });
-      }
+    const servicosComNotas = atendimento.servicos.map(servico => {
+      return {
+        id_servico: servico.id_servico,
+        nota: servico.nota
+      };
     });
 
     return {
       id,
       id_cliente: atendimento.id_cliente,
-      servicos,
+      servicos: servicosComNotas,
       horario_inicio: atendimento.horario_inicio,
       horario_fim: atendimento.horario_fim,
       preco: atendimento.preco,
       status_pagamento: atendimento.status_pagamento,
-    };
-  }
-};
-
-
-
-let nextId = 1;
-
-const model = (atendimento, id = nextId++) => {
-  if (
-    atendimento.id_cliente != undefined &&
-    clientes_controller.show(atendimento.id_cliente) &&
-    atendimento.id_servico != undefined &&
-    servicos_controller.show(atendimento.id_servico)
-  ) {
-    return {
-      id,
-      id_cliente: atendimento.id_cliente,
-      servicos: [
-        { id_servico: atendimento.id_servico, nota: atendimento.notas },
-      ],
-      horario_inicio: atendimento.horario_inicio,
-      horario_fim: atendimento.ihorario_fim,
-      preco: atendimento.id_cliente,
-      status_pagamento: atendimento.id_cliente,
     };
   }
 };
